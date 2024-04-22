1c71e42c143a75ea4e3f499f61bb0c3e789bb095
Арсений <dgwary@ya.ru>
2024-04-22 13:55:33 +0300

    Добавил страницы

---
+++ PetShopWeb/Views/Home/Tovar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PetShopWeb.Views.Home
+{
+    public class TovarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
