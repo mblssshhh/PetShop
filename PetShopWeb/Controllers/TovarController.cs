1b34d31e01b9abe8d088bea0faee9496a6316aee
Арсений <dgwary@ya.ru>
2024-05-02 17:12:39 +0300

    Сделал каталог, но нужно его подровнять. контактов нет. логин и регистрация сделаны.

---
+++ PetShopWeb/Controllers/TovarController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PetShopWeb.Controllers
+{
+    public class TovarController : Controller
+    {
+
+        public IActionResult Tovar()
+        {
+            return View();
+        }
+    }
+}
